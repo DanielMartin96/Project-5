18808c6a10be87f37251d420c238e96106c6de4f
Daniel Martin <danielmartin1996@hotmail.com>
2020-10-20 10:57:59 +0100

    10:57

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const getData = () => {
     xhr.open('GET', url);
     xhr.onload = () => {
         const data = JSON.parse(xhr.response);
-        for (i = 0; i<12; i++) {
+        for (i = 0; i<12; i++) { // cycles through the json data and appends two divs to the page
             const gallery = document.getElementById('gallery');
-            let html = document.createElement('div');
-            html.innerHTML = `<div class="card" id="${i}">
+            let cardDiv = document.createElement('div');
+            cardDiv.innerHTML = `<div class="card" id="${i}">
             <div class="card-img-container">
               <img class="card-img" src= ${data.results[i].picture.large} alt="profile picture">
             </div>
@@ -15,8 +15,31 @@ const getData = () => {
               <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
               <p class="card-text">${data.results[i].email}</p>
               <p class="card-text cap">${data.results[i].location.city}</p>
-            </div>
-            <div class="modal-container" id="${i}" style="display:none">
+            </div>`
+            let modalDiv = document.createElement('div'); // modal div style=display = none - meaning it is not shown on page
+            let phoneNumber = formatNumber(data.results[i].cell);
+            function formatNumber(number) { // formats the nuumber using regex
+              var cleaned = ('' + number).replace(/\D/g, '')
+              var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+              if (match) {
+                return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+              }
+              return null
+          }
+          let date = data.results[i].dob.date; // cycles through the date to find the numbers needed then concatinated them in the required way
+          const day1 = date[8]
+          const day2 = date[9]
+          const day = day1 + day2;
+          const month1 = date[5];
+          const month2 = date[6];
+          const month = month1 + month2;
+          const year1 = date[0];
+          const year2 = date[1];
+          const year3 = date[2];
+          const year4 = date[3];
+          const year = year1 + year2 + year3 + year4;
+          const bday = month + "/" + day + "/" + year;
+            modalDiv.innerHTML = `<div class="modal-container" id="${i}" style="display:none">
                         <div class="modal">
                             <button id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                             <div class="modal-info-container">
@@ -25,20 +48,22 @@ const getData = () => {
                                 <p class="modal-text">${data.results[i].email}</p>
                                 <p class="modal-text cap">${data.results[i].location.city}</p>
                                 <hr>
-                                <p class="modal-text">${data.results[i].cell}</p>
+                                <p class="modal-text">${phoneNumber}</p>
                                 <p class="modal-text">${data.results[i].location.street.number} ${data.results[i].location.street.name}, ${data.results[i].location.state}, ${data.results[i].location.postcode}</p>
-                                <p class="modal-text">Birthday: ${data.results[i].dob.date}</p>
+                                <p class="modal-text">Birthday: ${bday}</p>
                             </div>
                         </div>`
-            gallery.appendChild(html);
+            gallery.appendChild(cardDiv);
+            gallery.appendChild(modalDiv);
         }
-        for (i = 0; i< 12; i++) {
+        for (i = 0; i< 12; i++) { // makes each card clickable and opens the modal container
             const card = document.getElementsByClassName('card')[i];
+            const modalDiv = document.getElementsByClassName('modal-container')[i]
             card.addEventListener('click', () => {
-            card.lastChild.style.display = 'block';
+            modalDiv.style.display = 'block';
             })
           }
-          for (i = 0; i< 12; i++) {
+          for (i = 0; i< 12; i++) { // makes the close button clickable and closes the modal
             const button = document.getElementsByClassName('modal-close-btn')[i];
             button.addEventListener('click', (e) => {
                 e.target.parentNode.parentNode.parentNode.style.display = 'none';
